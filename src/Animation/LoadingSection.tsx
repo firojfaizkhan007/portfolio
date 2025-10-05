00b4c7fe86cb8c4c96fee7f13a15b7dc1062a151
Mohammed Danish <webmobril-backend>
2025-10-05 15:06:42 +0530

    remove

--- src/Animation/LoadingSection.tsx
+++
@@ -1,7 +0,0 @@
-const LoadingSection = () => (
-  <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 relative">
-    <div className="loader"></div>
-  </div>
-);
-
-export default LoadingSection;
